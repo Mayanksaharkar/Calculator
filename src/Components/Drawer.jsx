1cc59aad6e360f8ca35ea98e37d0a09ca2efc873
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-01-23 14:31:28 +0530

    header text size Changed

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -31,15 +31,15 @@ function Drawer() {
   }
 
   return (
-    <div className="drawer drawer-end">
+    <div className="drawer drawer-end ">
   <input id="my-drawer" type="checkbox" className="drawer-toggle" />
    
-  <div className="drawer-side">
+  <div className="drawer-side ">
     <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content rounded-s-3xl">
         {themes.map((theme)=>(
             <li  key={theme} onClick={e=>{ e.preventDefault(); handleThemeClick(theme)}}>
-                <div data-theme={theme}  className="flex flex-row justify-between menu  px-1 capitalize text-xl m-2 cursor-pointer rounded-xl hover:text-accent">
+                <div data-theme={theme}  className="flex flex-row justify-between menu py-2  px-1 capitalize text-sm m-2 cursor-pointer rounded-xl hover:text-accent">
                 <span className="ml-3 font-semibold">
                 {theme}
                 </span>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="flex justify-center py-2 z-0 ">
       <div className="navbar w-11/12  bg-base-300 shadow-md rounded-full px-4">
         <div className="navbar-start">
-          <a className="btn btn-ghost text-xl rounded-full ">Calculator</a>
+          <a className="btn btn-ghost text-2xl rounded-full ">Calculator</a>
         </div>
         <div className="navbar-end">
           <label htmlFor="my-drawer" className="btn btn-ghost rounded-full mr-4 text-xl bg-base-100" >Themes</label>
